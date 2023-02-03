cc5f1e1bf61e1a8be5144aad13498a43bef6bfea
Danielle <danielle@kendallspace.net>
2023-02-03 11:55:04 -0500

    add checkbox and demo useEffect hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Counter from "./components/counter";
 import Header from "./components/header";
 
 function App() {
+  let [checkboxValue, setCheckboxValue] = useState(false);
+
+  // count site visits
+  useEffect(() => {
+    console.log("first render, incrementing site visits by 1");
+  }, []);
+
+  console.log("rendering App"); // happens every rerender
+
+  let handleChange = (e) => {
+    setCheckboxValue(!checkboxValue);
+  };
+
   return (
     <div className="App">
       <Header />
       <Counter />
+      <input
+        type="checkbox"
+        value={checkboxValue}
+        onChange={() => handleChange()}
+      />
     </div>
   );
 }
