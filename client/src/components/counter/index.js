0c56c81f3f24d4d8fc7b60f6a7b14a1298afb1e8
Danielle <danielle@kendallspace.net>
2023-02-03 10:15:19 -0500

    create Counter component and import into App.js

---
+++ client/src/components/counter/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <span>Current Count: 0</span>
+      <section>
+        <button>+</button>
+        <button>-</button>
+      </section>
+    </div>
+  );
+};
+
+export default Counter;
