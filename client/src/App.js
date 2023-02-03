8771a08a5db2f24e60d8294a923fe4c322b94e41
Danielle <danielle@kendallspace.net>
2023-02-03 10:28:18 -0500

    add Header, update styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Counter from "./components/counter";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Counter />
     </div>
   );
---
+++ client/src/components/header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h1>Let's Get Counting!</h1>;
+};
+
+export default Header;
